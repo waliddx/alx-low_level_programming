b82a20e7e0748961e298b701135237366240bf75
walid gzouly <wagzouly@gmail.com>
2023-07-18 05:01:12 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print _putchar
+ * Code by: Prince Solomon
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
