c1766fd93ab5a292528f0c4522daf0386d0b7291
walid gzouly <wagzouly@gmail.com>
2023-07-18 05:35:06 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,15 +1,28 @@
 #include "main.h"
 
 /**
- * _isalpha - check if a character is alphabet
- * @c: the character to be check
- * prince Solomon
- * Return: 1 if character is letter, lowercase or upper, o upper
+ * print_sign - prints the sign of anumber.
+ * @n: The of which the sign will be printed.
+ *
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
  */
-int _isalpha(int c)
+int print_sign(int n)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (n > 0)
+	{
+		_putchar('+');
 		return (1);
-	else
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
 		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
